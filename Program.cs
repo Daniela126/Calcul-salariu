4ffda222fb66d4248e6d9ac4843395ae7d867e13
Daniela126 <daniela.dumitrita23@gmail.com>
2026-03-04 22:19:08 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        int oreLucrate;
+        double tarifPeOra;
+        double salariu;
+
+        
+        Console.Write("Introduceti numarul de ore lucrate: ");
+        oreLucrate = Convert.ToInt32(Console.ReadLine());
+
+        
+        Console.Write("Introduceti tariful pe ora: ");
+        tarifPeOra = Convert.ToDouble(Console.ReadLine());
+
+        
+        salariu = oreLucrate * tarifPeOra;
+
+       
+        Console.WriteLine("Salariul este: " + salariu + " lei");
+
+        
+        if (salariu > 3000)
+        {
+            Console.WriteLine("Salariu mare");
+        }
+        else
+        {
+            Console.WriteLine("Ati lucrat prea putine ore pentru a avea un salariu mare!");
+        }
+
+        Console.ReadLine();
+    }
+}
